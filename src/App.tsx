3e77f91e6dd03a4830b729e5428bbe049666e7ed
Vinay Sharma <vinay@geekyants.com>
2020-07-08 11:49:25 +0530

    firebase invisible reCaptcha MCVE added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useCallback } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+// added reCaptchaVerifier type declaration to Window variable
+declare global {
+  interface Window {
+    reCaptchaVerifier: unknown;
+  }
 }
 
-export default App;
+export default () => {
+  useEffect(() => {
+    // firebase initialization when App mounts
+    firebase.initializeApp({
+      apiKey: 'xxx',
+      authDomain: 'xxx',
+      databaseURL: 'xxx',
+      projectId: 'xxx',
+      storageBucket: 'xxx',
+      messagingSenderId: 'xxx',
+      appId: 'xxx',
+      measurementId: 'xxx',
+    });
+
+    // initialize firebase reCaptcha
+    window.reCaptchaVerifier = new firebase.auth.RecaptchaVerifier('login-button', {
+      size: 'invisible',
+      callback: () => {
+        /**
+         * ! callback is not being fired when we click on login button
+         * * same is working as expected in Vanilla JS in the repository below
+         * * https://github.com/vinaysharma14/firebase-invisible-reCaptcha/blob/master/index.html
+         */
+      },
+    });
+  }, [])
+
+  const handleSubmit = useCallback((event) => { event.preventDefault() }, [])
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input />
+      <button id="login-button">Submit</button>
+    </form>
+  )
+}
