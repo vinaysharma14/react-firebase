49f6f4602ac48e8452c4480f0f592e082593c04c
Nayan Jyoti Das <nayanj@geekyants.com>
2020-07-08 13:46:45 +0530

    Make the recaptcha work

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useState } from "react";
 
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/auth";
+
+// firebase initialization when App mounts
+firebase.initializeApp({
+  apiKey: "AIzaSyAWBBLDXcJcTZl6p4DFzSk-CFGIO6TZFSc",
+  authDomain: "xxx",
+  databaseURL: "xxx",
+  projectId: "fir-app-da285",
+  storageBucket: "xxx",
+  messagingSenderId: "xxx",
+  appId: "xxx",
+  measurementId: "xxx",
+});
 
 // added reCaptchaVerifier type declaration to Window variable
 declare global {
@@ -11,38 +23,60 @@ declare global {
 }
 
 export default () => {
+  const [
+    verifier,
+    setVerifier,
+  ] = useState<firebase.auth.RecaptchaVerifier | null>(null);
+
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   useEffect(() => {
-    // firebase initialization when App mounts
-    firebase.initializeApp({
-      apiKey: 'xxx',
-      authDomain: 'xxx',
-      databaseURL: 'xxx',
-      projectId: 'xxx',
-      storageBucket: 'xxx',
-      messagingSenderId: 'xxx',
-      appId: 'xxx',
-      measurementId: 'xxx',
-    });
+    setVerifier(
+      new firebase.auth.RecaptchaVerifier("recaptcha", {
+        // size: "invisible",
+        callback: () => {
+          /**
+           *It is getting called.
+           */
+          console.log("Not a robot.");
+        },
+      })
+    );
+  }, []);
 
-    // initialize firebase reCaptcha
-    window.reCaptchaVerifier = new firebase.auth.RecaptchaVerifier('login-button', {
-      size: 'invisible',
-      callback: () => {
-        /**
-         * ! callback is not being fired when we click on login button
-         * * same is working as expected in Vanilla JS in the repository below
-         * * https://github.com/vinaysharma14/firebase-invisible-reCaptcha/blob/master/index.html
-         */
-      },
-    });
-  }, [])
+  const handleSubmit = async () => {
+    console.log(`Got phone number as -> ${phoneNumber}`);
 
-  const handleSubmit = useCallback((event) => { event.preventDefault() }, [])
+    try {
+      if (verifier) {
+        const response = await firebase
+          .auth()
+          .signInWithPhoneNumber(phoneNumber, verifier);
+        console.log(response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input />
-      <button id="login-button">Submit</button>
-    </form>
-  )
-}
+    <>
+      {/* <form onSubmit={handleSubmit}>
+        <input />
+        <button id="login-button">Submit</button>
+      </form> */}
+      <input
+        type="text"
+        onChange={(event) => setPhoneNumber(event.target.value)}
+      ></input>
+      <button onClick={handleSubmit}>SubmitPhone</button>
+      <div
+        id="recaptcha"
+        style={{
+          width: 100,
+          height: 100,
+        }}
+      ></div>
+    </>
+  );
+};
